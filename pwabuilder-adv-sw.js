43f8853a7490856bedc79718141a753909144847
LeonardoFreedGamer <159263884+LeonardoFreedGamer@users.noreply.github.com>
2024-03-30 13:23:40 -0300

    app

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"0b0eecb0328bb2086e46b6ee2c31bf93","url":"index.html"},{"revision":"b4705d19f14d4d1819023e79b7365315","url":"style.css"}]);
